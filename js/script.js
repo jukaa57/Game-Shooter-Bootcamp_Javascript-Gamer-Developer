fd0d7b43042bf98e010965a5c0947b6a0c937770
Júnior O <junioroliveira.1150@gmail.com>
2021-08-29 23:04:18 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function start() {
+    $("#initiate").hide();
+
+    $("#backgroundGame").append("<div id='player' class='anima1'></div>");
+    $("#backgroundGame").append("<div id='enemies1'></div>");
+    $("#backgroundGame").append("<div id='enemies2'></div>");
+    $("#backgroundGame").append("<div id='ryan'></div>");
+
+}
